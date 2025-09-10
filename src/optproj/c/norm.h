0bf8524985dbb77907df1b0b2b29ff436daef731
JoernStoehler <124216958+JoernStoehler@users.noreply.github.com>
2025-09-10 08:30:10 +0000

    update

--- src/optproj/c/norm.h
+++ src/optproj/c/norm.h
@@ -3,4 +3,30 @@
 
 double l2_norm(const double *arr, int n);
 
+void aspect_ratio_convex_polygon(
+    // Vertices
+    const int n,
+    const double x[], // [n]
+    const double y[], // [n]
+
+    // Polygon
+    const int s, // number of vertices in polygon
+    const int p[], // indices of vertices, [n]
+
+    double *L, // aspect ratio, output
+    double dLdx[], // gradient w.r.t. x, output, [s]
+    double dLdy[]  // gradient w.r.t. y, output, [s]
+);
+
+void aspect_ratio_partition(
+    const int n,
+    const double x[], // [n]
+    const double y[], // [n]
+
+    const int m, // number of polygons
+    const int max_s, // max number of vertices in a polygon
+    const int p[], // indices of vertices, [m*max_s]
+
+)
+
 #endif
